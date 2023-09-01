73c87a368309b16b98a90eacbd54de3a415bd575
Diego Elián Rodríguez Cantú <75437643+DiegoElian02@users.noreply.github.com>
2023-09-01 08:58:49 -0600

    fixed some bugs

--- spam_rf.py
+++ spam_rf.py
@@ -10,16 +10,12 @@ import pickle
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
-from sklearn import metrics
-from sklearn.naive_bayes import GaussianNB
-
-def classificador(mensaje, classifier):
+def spam_clasificador(mensaje, modelo):
     mssgdata = pd.read_csv('spam.csv', encoding='latin-1')
     X, y = mssgdata.v2, mssgdata.v1
+    X = pd.Series(np.concatenate((X, [mensaje])))
     documents = []
 
     from nltk.stem import WordNetLemmatizer
@@ -27,34 +23,25 @@ def classificador(mensaje, classifier):
     stemmer = WordNetLemmatizer()
 
     for sen in range(0, len(X)):
-
         document = re.sub(r'\W', ' ', str(X[sen]))
-
         document = re.sub(r'\s+[a-zA-Z]\s+', ' ', document)
-
         document = re.sub(r'\^[a-zA-Z]\s+', ' ', document) 
-
         document = re.sub(r'\s+', ' ', document, flags=re.I)
-
         document = re.sub(r'^b\s+', '', document)
-
         document = document.lower()
-
         document = document.split()
 
         document = [stemmer.lemmatize(word) for word in document]
         document = ' '.join(document)
         
         documents.append(document)
-    
-    vectorizer = CountVectorizer(max_features=10000, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
+        
+    vectorizer = CountVectorizer(max_features=10000, min_df=1, max_df=0.6, ngram_range=(1,2), stop_words=stopwords.words('english'))
     X = vectorizer.fit_transform(documents).toarray()
 
     tfidfconverter = TfidfTransformer()
     X = tfidfconverter.fit_transform(X).toarray()
 
-    mensajetransformado = X[-1]
-    X = X[:(len(X)-1)]
+    y_p = modelo.predict(X[-1].reshape(1, -1))
 
-    y_pred = classifier.predict([mensajetransformado])
-    return y_pred
+    return y_p
